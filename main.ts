1f57e182b698d30333766aaac5c6d5125f7fd8de
1233451234567890 <149983844+1233451234567890@users.noreply.github.com>
2023-11-06 13:28:23 +1100

    I made sure Minimum Temperature works!

--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(0)
+    basic.showNumber(Maximum_Temperature)
 })
-let minimum_temperature = -5
-let Maximum_Temperature = 50
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(minimum_temperature)
+})
+let minimum_temperature = 0
+let Maximum_Temperature = 0
+Maximum_Temperature = 50
+minimum_temperature = -5
 basic.forever(function () {
-    if (input.temperature() > minimum_temperature) {
+    if (input.temperature() < minimum_temperature) {
         Maximum_Temperature = input.temperature()
+    } else {
+        minimum_temperature = input.temperature()
     }
 })
